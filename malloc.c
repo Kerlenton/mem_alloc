d53b4b7d8338609b2c52a99046a957ea44a4d973
Kerlenton <semyonschv@yandex.ru>
2024-01-24 15:39:35 +0300

    adding malloc

---
+++ malloc.c
@@ -0,0 +1,31 @@
+#include "common.c"
+
+void *malloc(size_t size) {
+    struct block_meta *block;
+    // TODO: align size?
+    if (size <= 0) {
+        return NULL;
+    }
+    if (!global_base) { // first call
+        block = request_space(NULL, size);
+        if (!block) {
+            return NULL;
+        }
+        global_base = block;
+    } else {
+        struct block_meta *last = global_base;
+        block = find_free_block(&last, size);
+        if (!block) { // failed to find free block
+            block = request_space(last, size);
+            if (!block) {
+                return NULL;
+            }
+        } else { // found free block
+            // TODO: consider splitting block here
+            block->free = 0;
+            block->magic = 0x77777777;
+        }
+    }
+    return (block + 1);
+}
+
