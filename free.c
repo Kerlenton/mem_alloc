d1d8c3a66f1cb8d1cf5ece9c1a65504882b5f01b
Kerlenton <semyonschv@yandex.ru>
2024-01-24 15:46:54 +0300

    adding free

---
+++ free.c
@@ -0,0 +1,13 @@
+#include "common.c"
+
+void free(void *ptr) {
+    if (!ptr) {
+        return;
+    }
+    // TODO: consider merging blocks once splitting blocks is implemented
+    struct block_meta *block_ptr = get_block_ptr(ptr);
+    assert(block_ptr->free == 0);
+    assert(block_ptr->magic == 0x77777777 || block_ptr->magic == 0x12345678);
+    block_ptr->free = 1;
+    block_ptr->magic = 0x55555555;
+}
